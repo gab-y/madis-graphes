7f10f06d7ebd23dc49c991040e7bd97aa5660538
gab-y <gabriel.guilbart@gmail.com>
2014-03-09 14:25:54 +0100

    Ajout de fermeture transitive

--- mainActivity.java
+++ mainActivity.java
@@ -12,22 +12,53 @@ public class mainActivity {
 		//FormatGraphe graph = new FormatGraphe(new File("./graph.dot"));
 		//int[][] matrice = graph.getMatrice();
 		int[][] matrice ={{0, 0, 1, 1, 0},{1,1,1,0,0},{0,1,0,0,0},{1,1,0,0,0},{1,0,0,1,0}};
-		FormatGraphe graph = new FormatGraphe(matrice);
+		
+		//FormatGraphe graph = new FormatGraphe(matrice);
+		affiche(matrice);
+
+		fermetureTransitive(matrice);
+		
+		affiche(matrice);
+		//graph.ecrireGraphe("./newGraph.dot");
+	}
+	
+	public static void affiche(int[][] matrice){
+		int longueur = matrice.length;
+		System.out.println();
 		System.out.print("    ");
 		for(int i=1 ; i<=5 ; i++){
 			System.out.print(i+ " ");
 		}
 		System.out.println();
-		for(int i=0 ; i<5 ; i++){
+		for(int i=0 ; i<longueur ; i++){
 			System.out.print(i+1 + " : ");
-			for(int j : matrice[i]){
+			for(int j=0; j<longueur ; j++){
 				System.out.print(matrice[i][j]+ " ");
 			}
 			System.out.println();
 		}
-		
-		
-		//graph.ecrireGraphe("./newGraph.dot");
+	}
+	
+	public static void fermetureTransitive (int[][] matrice){
+		int longueur = matrice.length;
+		boolean tryagain = true;
+		while(tryagain){
+			tryagain = false;
+			for(int i=0 ; i<longueur ; i++){
+				for(int j=0; j<longueur ; j++){
+					if(matrice[j][i]>0){//on lit par colonne (= origine). Si une destination existe...
+						for(int k=0; k<longueur; k++){
+							if(matrice[i][k]>0){//...on lit la ligne des noeuds ayant pour destination (= ligne) l'origine ci-dessus
+								if(matrice[j][k]<1){
+									matrice[j][k]=1;//quand c'est le cas, on met à 1 le croisement de l'origine et de la destination ci-dessus
+									tryagain = true;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
